c6ea9174d574562a1bef1565351515747e5c7769
Stuart McCann <smccann74@qub.ac.uk>
2025-08-03 19:39:01 +0100

    [JPS]: relative paths for gh pages

--- assets/js/include-head.js
+++ assets/js/include-head.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('../../components/head.html')
+    fetch('/JPS/components/head.html')
         .then(response => response.text())
         .then(data => {
             document.head.innerHTML = data;
