2bc08ed69ca204023e5a99841f44364d0473c994
Stuart McCann <smccann74@qub.ac.uk>
2025-10-12 18:13:26 +0100

    [JPS] : add base site structure

--- assets/js/include-head.js
+++ assets/js/include-head.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/JPS/components/head.html')
-        .then(response => response.text())
-        .then(data => {
-            document.head.innerHTML = data;
-        })
-        .catch(error => console.error('Error loading header:', error));
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     fetch('components/head.html')
+//         .then(response => response.text())
+//         .then(data => {
+//             document.head.innerHTML = data;
+//         })
+//         .catch(error => console.error('Error loading header:', error));
+// });
